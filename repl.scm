a4b081a8d81ade80cf18f00373fe215a9da2ff67
Ben Weaver <weaver@coptix.com>
2008-02-15 14:45:11 -0500

    added repl.scm (a bunch of config/load statements for working in the REPL)

---
+++ repl.scm
@@ -0,0 +1,6 @@
+,config ,load utility/s48-interfaces.scm
+,config ,load interfaces.scm
+,config ,load packages.scm
+,config ,load comp-interfaces.scm
+,config ,load comp-packages.scm
+,open octothorpe-extensions
