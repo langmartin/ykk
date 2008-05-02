1fe2f8df206d43c5d06c32e0a5ae77e7940016af
Lang Martin <lang@lang.coptix.lan>
2008-05-02 11:14:36 -0400

    end of the night

--- pre-post-eval.scm
+++ pre-post-eval.scm
@@ -1,29 +1,5 @@
 ;;;; ,open sxml-tools ,open sxml-tree-trans
 
-(define *tree* (call-with-input-file "/Users/lang/tmp/parsed-rss.scm" read))
-
-(define sample
-  '(begin*
-    (*text*
-     (lambda x #f))
-    (*default*
-     (lambda x (cdr x)))
-    (link (@ (order preorder))
-          (lambda x x))
-    (guid (@ (order preorder))
-          link)))
-
-;; (pre-post-order *tree*
-;;                 `((*text* . ,(lambda x #f))
-;;                   (*default* . ,(lambda x (cdr x)))
-;;                   (link *preorder* . ,(lambda x x))))
-
-(define (unsxml block)
-  (values
-   (car block)
-   (or (sxml:attr-list-node block) '(@))
-   (car (sxml:content-u block))))
-
 (define (sxml:content-u form)
   (cond ((null? (cdr form))
          '())
@@ -45,63 +21,13 @@
        .
        body))))
 
-;; (define (do-lambda env name binding . body)
-;;   (let (new-env (cons-env (zip binding binding) env))
-;;     `(,name
-;;       ,binding
-;;       ,@(map (lambda (expr)
-;;                (evaluate expr new-env))
-;;              body))))
-
-;; ;; (zip '(1 2 3) '(a b c))
-
-;; (define (lookup-in-env sym env)
-;;   (call-with-current-continuation
-;;    (lambda (found)
-;;      (for-each (lambda (env)
-;;                  (let ((val (sxml:content sym env)))
-;;                    (if val
-;;                        (found val))))
-;;                env)
-;;      sym)))
-
-;; (define (evaluate lst env)
-;;   (map (lambda (obj)
-;;          (cond ((lambda? obj)
-;;                 (apply do-lambda env obj))
-;;                ((symbol? obj)
-;;                 (lookup-in-env obj env))
-               
-;;                ))))
-
-;; (define-syntax define-runtime-syntax
-;;   (syntax-rules ()
-;;     ((_ name rules)
-;;      (define name
-;;        (let ((transform (car rules)))
-;;          (lambda form
-;;            (let* ((eval-form (cons 'name form))
-;;                   (result (transform eval-form identity eq?)))
-;;              (if (eq? result eval-form)
-;;                  (error 'runtime-syntax-error
-;;                         'name
-;;                         form)
-;;                  result))))))))
-
-;; (define-runtime-syntax example
-;;   (syntax-rules ()    
-;;     ((_ a b)
-;;      (list a b))
-;;     ((_ a)
-;;      (example "1-ary" a))))
-
 (define (document->let-body body)
   (list 'quasiquote
    (fold-right
     (lambda (handler acc)
       (let ((sym attr lambda-lst (unsxml handler)))
         (let-unspec* (cdr attr) ((order #f))
-          (let* ((proc (list 'unquote sym)))            
+          (let* ((proc (list 'unquote sym)))
             (cons (if order
                       (cons* sym (concat->symbol #\* order #\*) proc)
                       (cons* sym proc))
@@ -129,13 +55,49 @@
    form
    (document->pre-post-handlers document)))
 
-;; (let* ((foo (lambda)) ...)
-;;   (list (cons* 'foo foo))
-;;   ...)
+(define *tree* (call-with-input-file "/Users/lang/tmp/parsed-rss.scm" read))
+
+(define sample
+  '(begin*
+    (*text*
+     (lambda x #f))
+    (*default*
+     (lambda x x))
+    (link (@ (order preorder))
+          (lambda x x))
+    (begin link)
+    (guid (@ (order preorder))
+          link)
+    (bar (@ (order preorder))
+         (lambda (tag . rest)
+           (cons (concat->symbol 's: tag)
+                 rest)))
+    (foo (lambda x `(other-tag ...)))))
 
 (document->let-body sample)
 (document->let-parameters sample)
 (p
  (document->pre-post-handlers sample))
 
-(p (pre-post *tree* sample))
+(p (pre-post
+    `(begin
+       (foo (@ (bar "value"))))
+    sample))
+
+(define (opened->let-parameters opened)
+  (map (lambda (binding)
+         (list (name (car binding))
+               (val (cdr (last-pair binding)))))
+       opened))
+
+(begin (@@ (*NAMESPACES* (s ,sample)))
+  (mumble ...)
+  (div (@@ (*NAMESPACES* (s2 ,sample2)))
+    stuff-evaluated-in-open-s2/s ...)
+  (foo (@ (bar "value"))))
+
+`(begin
+   (s:foo (@ (s:bar "value"))))
+;; FOO is applied
+`(begin
+   (other-tag ...))
